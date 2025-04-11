54ee300a2789470ca0228e5b27c22699fbee58f7
Dev <kanuj2151@gmail.com>
2025-04-11 14:49:56 +0530

    Set Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/next"
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +35,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
-                        
+            <Analytics />        
           </ThemeProvider>
         </body>
     </html>
